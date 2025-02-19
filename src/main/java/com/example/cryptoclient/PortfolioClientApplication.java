9ae8e4c517b8207fbc27026f9975bdf3d7be261b
petermv74 <petermv74@gmail.com>
2025-02-19 15:50:29 +0100

    First commit

---
+++ src/main/java/com/example/cryptoclient/PortfolioClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cryptoclient/PortfolioClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptoclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
